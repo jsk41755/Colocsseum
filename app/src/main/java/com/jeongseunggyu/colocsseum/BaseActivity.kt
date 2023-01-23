ad697462269db2c9992a04fe533878e09d9f4932
jsk41755 <jsk41755@naver.com>
2023-01-23 23:56:58 +0900

    Initial commit

---
+++ app/src/main/java/com/jeongseunggyu/colocsseum/BaseActivity.kt
@@ -0,0 +1,11 @@
+package com.jeongseunggyu.colocsseum
+
+import androidx.appcompat.app.AppCompatActivity
+
+abstract class BaseActivity : AppCompatActivity(){
+
+    val mContext = this
+
+    abstract fun setupEvents()
+    abstract fun setValues()
+}
---
+++ app/src/main/java/com/jeongseunggyu/colocsseum/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.jeongseunggyu.colocsseum
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setValues()
+        setupEvents()
+    }
+
+    override fun setupEvents() {
+        TODO("Not yet implemented")
+    }
+
+    override fun setValues() {
+        TODO("Not yet implemented")
+    }
+}
