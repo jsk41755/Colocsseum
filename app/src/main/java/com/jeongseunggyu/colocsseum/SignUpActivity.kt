1bf66e60b44c91fecb3121c623fe10a7af14dca9
jeongseung-gyu <jsk41755@naver.com>
2023-01-26 00:06:38 +0900

    회원가입 함수 추가 + 회원가입화면에서 활용

--- app/src/main/java/com/jeongseunggyu/colocsseum/SignUpActivity.kt
+++ app/src/main/java/com/jeongseunggyu/colocsseum/SignUpActivity.kt
@@ -2,17 +2,39 @@ package com.jeongseunggyu.colocsseum
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jeongseunggyu.colocsseum.databinding.ActivityMainBinding
+import com.jeongseunggyu.colocsseum.databinding.ActivitySignUpBinding
+import com.jeongseunggyu.colocsseum.utils.ServerUtil
+import org.json.JSONObject
 
 class SignUpActivity : BaseActivity() {
 
+    private lateinit var binding: ActivitySignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         setupEvents()
         setValues()
     }
 
     override fun setupEvents() {
+
+        binding.signUpBtn.setOnClickListener {
+            val inputEmail = binding.emailEdt.text.toString()
+            val inputPw = binding.passwordEdt.text.toString()
+            val inputNickname = binding.nicknameEdt.text.toString()
+
+            ServerUtil.putRequestSignUp(inputEmail, inputPw, inputNickname, object : ServerUtil.Companion.JsonResponseHandler{
+                override fun onResponse(jsonObj: JSONObject) {
+
+                }
+
+            })
+        }
+
     }
 
     override fun setValues() {
--- app/src/main/java/com/jeongseunggyu/colocsseum/utils/ServerUtil.kt
+++ app/src/main/java/com/jeongseunggyu/colocsseum/utils/ServerUtil.kt
@@ -70,5 +70,62 @@ class ServerUtil {
             })
         }
 
+        //회원가입 하는 기능
+        fun putRequestSignUp(email : String, pw : String, nick : String, handler : JsonResponseHandler?){
+//          서버에 입력받은 email, pw, nick 전달 => 로그인 기능 PUT / user 로 전달. => 요청(Request) 실행.
+            // 라이브러리 (okHTTP) 활용해서 짜보자
+
+            // http//54.180.52.26/user + PUT + 파라미터들 첨부.
+
+            //호스트 주소 + 기능 주소 결합
+            val urlString = "${BASE_URL}/user"
+
+            //PUT 방식 => 파라미터를 폼데이터(폼바디)에 담아두자.
+            val formData = FormBody.Builder()
+                .add("email", email)
+                .add("password", pw)
+                .add("nick_name", nick)
+                .build()
+
+            //어디로 => 어떻게 => 어떤 데이터를 들고 가는지를 모두 종합해둔, Request 변수 생성.
+            val request = Request.Builder()
+                .url(urlString)
+                .put(formData)
+                .build()
+
+            //클라이언트로써의 동작 : Request 요청 실행. => OkHTTP라이브러리 지원.
+            val client = OkHttpClient()
+
+            //실제로 서버에 요청 날리기 => 갔다 와서는 뭘 한건지?
+            client.newCall(request).enqueue(object : Callback{
+                override fun onFailure(call: Call, e: IOException) {
+                    //서버의 연결 자체를 실패한 경우.
+                    //서버 마비, 인터넷 단선. (로그인 실패X)
+                    Log.d("응답 본문", "에ㅔ레렐ㄹ레")
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    //로그인 성공, 로그인 실패(연결 성공 -> 검사 실패)
+                    //응답이 돌아온 경우.
+
+                    //응답 본문을 String으로 저장
+                    val bodyString = response.body!!.string()
+
+                    //bodyString 변수에는 한글이 깨져있다. => JsonObject로 변환하면, 한글 정상 처리.
+                    val jsonObj = JSONObject(bodyString)
+
+                    Log.d("응답 본문", jsonObj.toString())
+
+                    //handler 변수가 null이 아니라면, (실체가 있다면)
+                    //그 내부에 적힌 내용 실행.
+
+                    handler?.onResponse(jsonObj)
+                }
+
+            })
+        }
+
+
+
     }
 }
