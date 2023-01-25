7f09b9c638ffc5934e0cc11fff44c6f5f427b63c
jeongseung-gyu <jsk41755@naver.com>
2023-01-25 21:41:48 +0900

    로그인 버튼 눌리면 => 입력한 아이디/ 비번 가지고오기

--- app/src/main/java/com/jeongseunggyu/colocsseum/MainActivity.kt
+++ app/src/main/java/com/jeongseunggyu/colocsseum/MainActivity.kt
@@ -2,17 +2,31 @@ package com.jeongseunggyu.colocsseum
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jeongseunggyu.colocsseum.databinding.ActivityMainBinding
 
 class MainActivity : BaseActivity() {
 
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         setValues()
         setupEvents()
     }
 
     override fun setupEvents() {
+        binding.loginBtn.setOnClickListener {
+            //입력한 이메일 / 비밀번호가 뭔지 변수에 저장.
+            val inputEmail = binding.emailEdt.text.toString()
+            val inputPw = binding.passwordEdt.text.toString()
+
+            //서버에 실제 회원이 맞는지 확인 요청. (Request) ??
+
+        }
+
     }
 
     override fun setValues() {
