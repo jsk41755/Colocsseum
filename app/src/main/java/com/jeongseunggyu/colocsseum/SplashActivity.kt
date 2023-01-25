976e8c3fdbdb394b08f1b7c15f4594cf6d7960d3
jeongseung-gyu <jsk41755@naver.com>
2023-01-26 01:15:01 +0900

    간단한 자동로그인 마무리

--- app/src/main/java/com/jeongseunggyu/colocsseum/SplashActivity.kt
+++ app/src/main/java/com/jeongseunggyu/colocsseum/SplashActivity.kt
@@ -1,21 +1,53 @@
 package com.jeongseunggyu.colocsseum
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import com.jeongseunggyu.colocsseum.databinding.ActivitySignUpBinding
+import com.jeongseunggyu.colocsseum.databinding.ActivitySplashBinding
+import com.jeongseunggyu.colocsseum.utils.ContextUtil
 
 class SplashActivity : BaseActivity() {
 
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        setValues()
+        setupEvents()
     }
 
     override fun setupEvents() {
-        TODO("Not yet implemented")
+
     }
 
     override fun setValues() {
-        TODO("Not yet implemented")
+        //3초 후에 검사 -> 저장된 토큰이 있는지? 아니면, "" 인지?
+
+        val myHandler = Handler(Looper.getMainLooper())
+
+        myHandler.postDelayed({
+             if(ContextUtil.getToken(mContext) == ""){
+                 // 저장된 토큰이 없다 => 로그인을 해야한다.
+                 val myIntent = Intent(mContext, LoginActivity::class.java)
+                 startActivity(myIntent)
+             }
+            else{
+                //저장된 토큰이 있다 => 로그인이 되어있다. => 메인화면으로 이동.
+                 val myIntent = Intent(mContext, MainActivity::class.java)
+                 startActivity(myIntent)
+             }
+
+            finish()
+
+        }, 3000)
     }
 
 }
