6cf7211785e70c3c35e1688ff9949b53b1505fea
jeongseung-gyu <jsk41755@naver.com>
2023-01-25 22:16:34 +0900

    화면에서, 서버 응답을 분석/ 반영 할 준비 작업

--- app/src/main/java/com/jeongseunggyu/colocsseum/MainActivity.kt
+++ app/src/main/java/com/jeongseunggyu/colocsseum/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.jeongseunggyu.colocsseum.databinding.ActivityMainBinding
 import com.jeongseunggyu.colocsseum.utils.ServerUtil
+import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
 
@@ -25,7 +26,15 @@ class MainActivity : BaseActivity() {
             val inputPw = binding.passwordEdt.text.toString()
 
             //서버에 실제 회원이 맞는지 확인 요청. (Request)
-            ServerUtil.postRequestLogin(inputEmail, inputPw)
+            ServerUtil.postRequestLogin(inputEmail, inputPw, object : ServerUtil.Companion.JsonResponseHandler{
+                override fun onResponse(jsonObj: JSONObject) {
+                    //jsonObj : 서버에서 내려준 본문을 JSON 형태로 가공해준 결과물.
+                    // => 이 내부를 파싱(분석)해서, 상황에 따른 대응.
+                    // => ex. 로그인 실패시, 그 이유를 토스트로 띄운다던지.
+
+                }
+
+            })
         }
 
     }
