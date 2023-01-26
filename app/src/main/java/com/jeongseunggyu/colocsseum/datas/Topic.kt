cebf7192441118a2260809d33c5e8c7f3c672f60
jeongseung-gyu <jsk41755@naver.com>
2023-01-26 23:45:05 +0900

    주제 파싱시 -> 선택 진영 + 득표 현황 같이 파싱

--- app/src/main/java/com/jeongseunggyu/colocsseum/datas/Topic.kt
+++ app/src/main/java/com/jeongseunggyu/colocsseum/datas/Topic.kt
@@ -8,6 +8,8 @@ class Topic : java.io.Serializable{
     var title = "" //나중에 String이 들어올 것이라는 명시
     var imageURL = ""
 
+    val sides = ArrayList<Side>()
+
     companion object{
 
         //적당한 모양이 되어 있는 JsonObject 하나를 넣어 주면 => Topic 형태로 변환해주는 함수 작성
@@ -18,6 +20,18 @@ class Topic : java.io.Serializable{
             resultTopic.id = jsonObj.getInt("id")
             resultTopic.title = jsonObj.getString("title")
             resultTopic.imageURL = jsonObj.getString("img_url")
+
+            val sidesArr= jsonObj.getJSONArray("sides")
+
+            for (i in 0 until sidesArr.length()){
+
+                //토픽마다 하위정보로 달린 => 선택진영을 파싱.
+                val sidObj = sidesArr.getJSONObject(i)
+                val side = Side.getSideFromJson(sidObj)
+
+                resultTopic.sides.add(side)
+
+            }
 
             return resultTopic
 
