7bf0fce2d4ee67c4465feb095dfc71382276cafb
jeongseung-gyu <jsk41755@naver.com>
2023-01-26 22:02:39 +0900

    어댑터 추가

---
+++ app/src/main/java/com/jeongseunggyu/colocsseum/adapters/TopicAdapter.kt
@@ -0,0 +1,31 @@
+package com.jeongseunggyu.colocsseum.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import com.jeongseunggyu.colocsseum.R
+import com.jeongseunggyu.colocsseum.datas.Topic
+
+class TopicAdapter(
+    val mContext : Context,
+    resId : Int,
+    val mList : List<Topic>) : ArrayAdapter<Topic>(mContext, resId, mList){
+
+    val mInflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if (tempRow == null){
+            tempRow = mInflater.inflate(R.layout.topic_list_item, null)//루트는 부가정보
+        }
+
+        val row = tempRow!!
+
+        return row
+    }
+
+
+}
