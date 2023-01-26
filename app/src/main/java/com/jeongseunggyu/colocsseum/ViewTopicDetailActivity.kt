e00598637886ce958bca097178142db574e799b7
jeongseung-gyu <jsk41755@naver.com>
2023-01-26 23:19:32 +0900

    주제 상세 화면 초안 작성중  - 토론 진행 상황 (선택 진영 / 득표수) 미작업

--- app/src/main/java/com/jeongseunggyu/colocsseum/ViewTopicDetailActivity.kt
+++ app/src/main/java/com/jeongseunggyu/colocsseum/ViewTopicDetailActivity.kt
@@ -2,12 +2,22 @@ package com.jeongseunggyu.colocsseum
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.jeongseunggyu.colocsseum.databinding.ActivitySplashBinding
+import com.jeongseunggyu.colocsseum.databinding.ActivityViewTopicDetailBinding
+import com.jeongseunggyu.colocsseum.datas.Topic
 
 class ViewTopicDetailActivity : BaseActivity() {
 
+    lateinit var mTopic: Topic
+
+    private lateinit var binding: ActivityViewTopicDetailBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_view_topic_detail)
+        binding = ActivityViewTopicDetailBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         setValues()
         setupEvents()
     }
@@ -17,7 +27,12 @@ class ViewTopicDetailActivity : BaseActivity() {
     }
 
     override fun setValues() {
-        TODO("Not yet implemented")
+
+        mTopic = intent.getSerializableExtra("topic") as Topic
+
+        binding.topicTitleTxt.text = mTopic.title
+        Glide.with(mContext).load(mTopic.imageURL).into(binding.topicImg)
+
     }
 
 
