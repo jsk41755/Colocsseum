a548e6d32ba000895a46d2418e302ecb783b7beb
jeongseung-gyu <jsk41755@naver.com>
2023-01-27 22:21:33 +0900

    의견 작성하기 기능 마무리

--- app/src/main/java/com/jeongseunggyu/colocsseum/EditReplyActivity.kt
+++ app/src/main/java/com/jeongseunggyu/colocsseum/EditReplyActivity.kt
@@ -2,20 +2,66 @@ package com.jeongseunggyu.colocsseum
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.jeongseunggyu.colocsseum.databinding.ActivityEditReplyBinding
+import com.jeongseunggyu.colocsseum.databinding.ActivityViewTopicDetailBinding
+import com.jeongseunggyu.colocsseum.datas.Side
+import com.jeongseunggyu.colocsseum.utils.ServerUtil
+import org.json.JSONObject
 
 class EditReplyActivity : BaseActivity() {
 
+    lateinit var mSelectedSide : Side
+
+    private lateinit var binding: ActivityEditReplyBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_reply)
+        binding = ActivityEditReplyBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
         setupEvents()
         setValues()
     }
 
     override fun setupEvents() {
+
+        binding.okBtn.setOnClickListener {
+
+            val inputContent = binding.contentEdt.text.toString()
+
+            ServerUtil.postRequestReply(mContext, mSelectedSide.topicId, inputContent, object  : ServerUtil.Companion.JsonResponseHandler{
+                override fun onResponse(jsonObj: JSONObject) {
+
+                    val code = jsonObj.getInt("code")
+
+                    if(code == 200){
+                        Toast.makeText(mContext, "의견 등록에 성공했습니다.", Toast.LENGTH_SHORT).show()
+                        finish()
+
+                    }
+                    else{
+                        val message = jsonObj.getString("message")
+                        runOnUiThread {
+
+                            Toast.makeText(mContext,message, Toast.LENGTH_SHORT).show()
+
+                        }
+                    }
+
+                }
+
+            })
+
+        }
+
     }
 
     override fun setValues() {
+
+        mSelectedSide = intent.getSerializableExtra("mySide") as Side
+
+        binding.mySelectedSideTxt.text = mSelectedSide.title
     }
 
 
--- app/src/main/java/com/jeongseunggyu/colocsseum/utils/ServerUtil.kt
+++ app/src/main/java/com/jeongseunggyu/colocsseum/utils/ServerUtil.kt
@@ -304,6 +304,58 @@ class ServerUtil {
             })
         }
 
+        //의견 등록하기
+        fun postRequestReply(context: Context, topicId : Int, content: String, handler : JsonResponseHandler?){
+//          서버에 입력받은 email, pw 전달 => 로그인 기능 POST / user 로 전달. => 요청(Request) 실행.
+            // 라이브러리 (okHTTP) 활용해서 짜보자
+
+            // http//54.180.52.26/user + POST + 파라미터들 첨부.
+
+            //호스트 주소 + 기능 주소 결합
+            val urlString = "${BASE_URL}/topic_reply"
+
+            val formData = FormBody.Builder()
+                .add("topic_id", topicId.toString())
+                .add("content", content)
+                .build()
+
+            //어디로 => 어떻게 => 어떤 데이터를 들고 가는지를 모두 종합해둔, Request 변수 생성.
+            val request = Request.Builder()
+                .url(urlString)
+                .post(formData)
+                .header("X-Http-Token", ContextUtil.getToken(context))
+                .build()
+
+            val client = OkHttpClient()
+
+            client.newCall(request).enqueue(object : Callback{
+                override fun onFailure(call: Call, e: IOException) {
+                    //서버의 연결 자체를 실패한 경우.
+                    //서버 마비, 인터넷 단선. (로그인 실패X)
+                    Log.d("응답 본문", "에ㅔ레렐ㄹ레")
+                }
+
+                override fun onResponse(call: Call, response: Response) {
+                    //로그인 성공, 로그인 실패(연결 성공 -> 검사 실패)
+                    //응답이 돌아온 경우.
+
+                    //응답 본문을 String으로 저장
+                    val bodyString = response.body!!.string()
+
+                    //bodyString 변수에는 한글이 깨져있다. => JsonObject로 변환하면, 한글 정상 처리.
+                    val jsonObj = JSONObject(bodyString)
+
+                    Log.d("응답 본문", jsonObj.toString())
+
+                    //handler 변수가 null이 아니라면, (실체가 있다면)
+                    //그 내부에 적힌 내용 실행.
+
+                    handler?.onResponse(jsonObj)
+                }
+
+            })
+        }
+
 
     }
 }
