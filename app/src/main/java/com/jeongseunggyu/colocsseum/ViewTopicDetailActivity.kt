343e508af073cf1791543569d08b9a93ab32c8fe
jeongseung-gyu <jsk41755@naver.com>
2023-01-27 21:09:01 +0900

    응답 처리 -> 투표 현황 새로고침 -> 기존에 만들어둔 함수 재사용

--- app/src/main/java/com/jeongseunggyu/colocsseum/ViewTopicDetailActivity.kt
+++ app/src/main/java/com/jeongseunggyu/colocsseum/ViewTopicDetailActivity.kt
@@ -30,7 +30,10 @@ class ViewTopicDetailActivity : BaseActivity() {
 
             ServerUtil.postRequestVote(mContext,mTopic.sides[0].id, object : ServerUtil.Companion.JsonResponseHandler{
                 override fun onResponse(jsonObj: JSONObject) {
-                    //서버 응답 대응.
+                    //서버 응답 대응. => 서버에서 최신 투표 현황을 받아서, 다시 UI에 반영.
+                    // 만들어둔 함수 재활용
+
+                    getTopicDetailFromServer()
 
                 }
 
