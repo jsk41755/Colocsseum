d55c27650fc67d0a6f000b0b05480b929e720ee7
jeongseung-gyu <jsk41755@naver.com>
2023-01-27 23:45:42 +0900

    실제로 내 좋아요/싫어요 여부에 따른 UI 변경

--- app/src/main/java/com/jeongseunggyu/colocsseum/adapters/ReplyAdapter.kt
+++ app/src/main/java/com/jeongseunggyu/colocsseum/adapters/ReplyAdapter.kt
@@ -1,6 +1,8 @@
 package com.jeongseunggyu.colocsseum.adapters
 
 import android.content.Context
+import android.content.res.ColorStateList
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -48,6 +50,33 @@ class ReplyAdapter(
 
         likeCountBtn.text = "좋아요 ${data.likeCount}개"
         dislikeCountBtn.text = "싫어요 ${data.dislikeCount}개"
+
+        if(data.myLike){
+            //좋아요만. 글씨 빨간색 + 배경 빨간 테두리 싫어요 -> 회색
+            likeCountBtn.setBackgroundResource(R.drawable.red_border_box)
+            likeCountBtn.setTextColor(Color.parseColor("#ff0000"))
+
+            dislikeCountBtn.setBackgroundResource(R.drawable.gray_border_box)
+            dislikeCountBtn.setTextColor(Color.parseColor("#A0A0A0"))
+
+        }
+        else if(data.myDislke){
+            //싫어요만 글씨 파란색 + 배경도 파란 테두리 좋아요 -> 회색
+            likeCountBtn.setBackgroundResource(R.drawable.gray_border_box)
+            likeCountBtn.setTextColor(Color.parseColor("#A0A0A0"))
+
+            dislikeCountBtn.setBackgroundResource(R.drawable.blue_border_box)
+            dislikeCountBtn.setTextColor(Color.parseColor("#0000FF"))
+        }
+        else{
+            //좋아요 / 싫어요 찍지 않은 상태. -> 둘다 회색
+            likeCountBtn.setBackgroundResource(R.drawable.gray_border_box)
+            likeCountBtn.setTextColor(Color.parseColor("#A0A0A0"))
+
+            dislikeCountBtn.setBackgroundResource(R.drawable.gray_border_box)
+            dislikeCountBtn.setTextColor(Color.parseColor("#D0D0D0"))
+
+        }
 
         likeCountBtn.setOnClickListener{
 //          좋아요 API 호출
