2b1259e54e315a00e2f6e7d7f7193292d2fb06ec
jeongseung-gyu <jsk41755@naver.com>
2023-01-27 22:28:35 +0900

    의견 파싱할 때 -> 선택 진영이 어딘지도 같이 파싱

--- app/src/main/java/com/jeongseunggyu/colocsseum/datas/Reply.kt
+++ app/src/main/java/com/jeongseunggyu/colocsseum/datas/Reply.kt
@@ -7,6 +7,8 @@ class Reply {
     var id = 0
     var content = ""
 
+    lateinit var selectedSide : Side
+
     companion object{
 
         fun getReplyFromJson(jsonObj: JSONObject) : Reply {
@@ -14,6 +16,8 @@ class Reply {
 
             resultReply.id = jsonObj.getInt("id")
             resultReply.content = jsonObj.getString("content")
+
+            resultReply.selectedSide = Side.getSideFromJson(jsonObj.getJSONObject("selected_side")) //Side형태로 타입 변환
 
 
             return resultReply
